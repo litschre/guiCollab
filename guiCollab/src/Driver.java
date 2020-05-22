53f4255f3b55cb881a929b7044b8691df288459c
litschre <litschre@dukes.jmu.edu>
2020-05-22 18:21:27 -0400

    Driver class

---
+++ guiCollab/src/Driver.java
@@ -0,0 +1,16 @@
+/**
+ * Driver class for GUI.
+ * @author Rachel Litscher and John Litscher
+ * @version 1.0
+ *
+ */
+public class Driver {
+
+    /**
+     * main method.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
