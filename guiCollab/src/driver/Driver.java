81d8517056c7d8c90ffd85de663a157036a04700
litschre <litschre@dukes.jmu.edu>
2020-05-22 21:41:31 -0400

    board set up

--- guiCollab/src/driver/Driver.java
+++ guiCollab/src/driver/Driver.java
@@ -4,17 +4,19 @@ import gui.Board;
 
 /**
  * Driver class for GUI.
+ * 
  * @author Rachel Litscher and John Litscher
  * @version 1.0
  *
  */
 public class Driver {
 
-    /**
-     * main method.
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        new Board();
-    }
+  /**
+   * main method.
+   * 
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    new Board();
+  }
 }
--- guiCollab/src/gui/Board.java
+++ guiCollab/src/gui/Board.java
@@ -4,6 +4,14 @@ import javax.swing.JFrame;
 
 public class Board extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * default constructor.
+   */
+  public Board() {
+
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
 }
